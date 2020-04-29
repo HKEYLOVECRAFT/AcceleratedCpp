b0f6c6263dd9224a89eddf128fbadeea681465ae
unknown <jkiviniitty@gmail.com>
2020-04-29 21:27:51 +0300

    adding exercises

--- chapter0/ex0-0.cpp
+++ chapter0/ex0-0.cpp
@@ -1,6 +1,7 @@
 // a small C++ program
 #include <iostream>
 
+/*
 int main()
 {	//0.0 ex (the original hello world program)
 	std::cout << "Hello, world!" << std::endl;
@@ -14,5 +15,42 @@ int main()
 	std::cout << "\t std::cout << \"Hello, world!\" << std::endl;\n";
 	std::cout << "\t return 0;\n";
 	std::cout << "}\n";
+
+	
 	return 0;
 }
+*/
+
+#include <iostream>
+int main() {{{{{{ std::cout << "Hello, world!" << std::endl; }}}}}}
+
+
+/* The rest of the exercises
+
+0-5. 
+Is this a valid program? Why or why not?
+
+#include <iostream>
+int main() std::cout << "Hello, world!" << std::endl;
+
+A 
+No. C++ needs brackets around any statement inside a function. Running this would get 
+error: expected initializer before ‘std’
+
+0-6. 
+Is this a valid program? Why or why not?
+
+#include <iostream>
+int main() {{{{{{ std::cout << "Hello, world!" << std::endl; }}}}}}
+
+A
+Yes. This version of the code has brackets. Actually 6 pairs of brackets and that is totally functional even though super messy way to write code.
+
+0-7
+Check the exercise from the book as it cannot be written here as it uses these /* free-from comments the wrong way. 
+
+0-8
+This excerise snippet cancels out my last free-form comment mark, so it cannot be copied. It is valid code though.
+
+
+*/
---
+++ notes.md
@@ -0,0 +1,8 @@
+Notes on Accelerated C++
+
+Ch. 0 Getting started
+
+
+<<          output operator
+std::       part of namespace named std
+std::cout   the name cout that is in the namespace scope std
